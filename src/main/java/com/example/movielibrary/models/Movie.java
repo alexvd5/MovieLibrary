a727ab768349939304d79b90e2fb0e7f95cb41fb
Aleksandar Plamenov <you@example.com>
2026-07-24 01:06:05 +0300

    Initial commit

---
+++ src/main/java/com/example/movielibrary/models/Movie.java
@@ -0,0 +1,11 @@
+package com.example.movielibrary.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Movie {
+    private int id;
+    private String title;
+    private Director Director;
+    private LocalDateTime releaseYear;
+}
