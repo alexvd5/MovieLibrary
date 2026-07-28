41b4fca9c457f08766351c771998355fa828dd16
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-07-28 23:33:42 +0300

    Adding annotations

--- src/main/java/com/example/movielibrary/services/CustomUserDetailsService.java
+++ src/main/java/com/example/movielibrary/services/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.example.movielibrary.services;
 import com.example.movielibrary.models.User;
 import com.example.movielibrary.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
