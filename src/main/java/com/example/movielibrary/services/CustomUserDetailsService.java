1719ffea158dc50bf89af61af4fc8337dd54d546
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-07-28 17:45:35 +0300

    Implementing CustomUserDetailsService

--- src/main/java/com/example/movielibrary/services/CustomUserDetailsService.java
+++ src/main/java/com/example/movielibrary/services/CustomUserDetailsService.java
@@ -22,7 +22,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
-        String roleName = "ROLE_" + user.getRole().name();
+        String roleName = user.getRole().name();
 
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
