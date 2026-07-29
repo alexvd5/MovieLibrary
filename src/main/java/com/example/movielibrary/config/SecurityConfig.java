054dafec4d87a15da3eda5b10b4769b16299747d
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-07-29 11:39:21 +0300

    Adding permissions for spring security for swagger to work

--- src/main/java/com/example/movielibrary/config/SecurityConfig.java
+++ src/main/java/com/example/movielibrary/config/SecurityConfig.java
@@ -27,6 +27,14 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/movies/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/movies/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/movies/**").hasRole("ADMIN")
+                        .requestMatchers(
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
 
                         .anyRequest().authenticated()
                 )
